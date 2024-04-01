4729d48159401bb65e758d8a5784ac97bcb142b0
praveen-klaytn <praveen.challa@klaytn.foundation>
2024-04-01 12:05:00 +0800

    updated cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 var request = require('request');
+const cors = require('cors');
 
 // Create Express Server
 const app = express();
-
+app.use(cors());
 // Configuration
 const PORT = 3000;
 const API_SERVICE_URL = "https://cryptobubbles.net/backend/data/bubbles1000.usd.json";
